ef557c827b86ac6849001b5eb7c38563b7b959c8
Brenda Franco and Susan Southall <susan@>
2018-03-29 16:28:42 -0700

    oops.

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,64 @@
 // Begin business logic.
+//
+// var numerals = [];
+//
+// function adjustArray(inputNumbers) {
+//   if (inputNumbers.length === 3) {
+//     var test = inputNumbers.unshift(0);
+//     return test;
+// } else if (inputNumbers.length === 2) {
+//     return inputNumbers.unshift(0, 0);
+// } else {
+//     return inputNumbers.unshift(0, 0, 0);
+// } alert(inputNumbers);
+// }
 
-var numerals = [];
 function convert(numbers) {
-  if (numbers.length === 4) {
-    debugger;
-    var insertM = "M".repeat(numbers[0]);
-    numerals.push(insertM);
-    return numerals.join("");
-    alert(numerals);
+// THOUSANDS
+  for(var i=0; i<numbers.length; i++) {
+    if (numbers.length === 4) {
+      numerals.push("M".repeat(numbers[i]));
+      return numerals.join("");
+      // var test = numbers.length-1;
+    }
+// HUNDREDS
+    else if (numbers.length === 3) {
+        if (numbers[i] <= 3) {
+          numerals.push("C".repeat(numbers[0]));
+          return numerals.join("");
 
-  } else if (numbers.length === 3) {
 
-  } else if (numbers.length === 2) {
+        } else if (numbers[i] > 4 && numbers[0] < 9) {
+          numerals.push("D", "C".repeat(numbers[0]-5));
+          return numerals.join("");
 
-  } else {
+        } else if (numbers[i] === 4) {
+          numerals.push("CD");
+          return numerals.join("");
 
-  }
+        } else if (numbers[i] === 9) {
+          numerals.push("CM");
+          return numerals.join("");
+        }
+// TENS
+    } else if (numbers.length === 2) {
+      numerals.push("C".repeat(numbers[0]));
+      return numerals.join("");
+// SINGLES
+    } else {
+
+    }
+
 }
+convert(300);
+alert(numerals);
+
+
+
+
+
+
+
 
 // Begin User interface logic.
 
